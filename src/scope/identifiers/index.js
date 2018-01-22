c2d441484a5467f0dff2c7d7e7828bbd2d004ac6
Matt Phillips <matt@mattphillips.io>
2018-01-22 14:09:03 +0000

    Update codebase with prettier

--- src/scope/identifiers/index.js
+++ src/scope/identifiers/index.js
@@ -1,7 +1,10 @@
 const { negate } = require('../../utils');
 
 const getIdentifiers = predicate => bindings =>
-  Object.keys(bindings).map(key => bindings[key]).filter(predicate).map(({ identifier }) => identifier);
+  Object.keys(bindings)
+    .map(key => bindings[key])
+    .filter(predicate)
+    .map(({ identifier }) => identifier);
 
 const isParam = binding => binding.kind === 'param';
 
